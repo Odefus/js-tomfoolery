8e699561db508cdd1a45deacd49a107ef741419b
Odefus <otto.koivisto@gmail.com>
2018-02-21 14:10:58 +0200

    implemented dice roll and added some dom initialization logic

--- DOM-Pig-Game/app.js
+++ DOM-Pig-Game/app.js
@@ -9,18 +9,26 @@ GAME RULES:
 
 */
 
-var scores, roundScores, dice;
+var scores, roundScores, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector("#current-" + activePlayer).textContent = dice;
-
 document.querySelector(".dice").style.display = "none";
 
+document.getElementById("score-0").textContent = '0';
+document.getElementById("score-1").textContent = '0';
+document.getElementById("current-0").textContent = '0';
+document.getElementById("current-1").textContent = '0';
 
+document.querySelector(".btn-roll").addEventListener("click", function() {
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    var diceDom = document.querySelector(".dice");
+
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + dice + ".png";
+});
 
 
