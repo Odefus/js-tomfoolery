9545fb63816c820b7e870cf6b4b03c5316942a36
Odefus <otto.koivisto@gmail.com>
2018-02-21 14:41:33 +0200

    additional game logic added

--- DOM-Pig-Game/app.js
+++ DOM-Pig-Game/app.js
@@ -23,28 +23,59 @@ document.getElementById("current-0").textContent = '0';
 document.getElementById("current-1").textContent = '0';
 
 document.querySelector(".btn-roll").addEventListener("click", function() {
+    // Get random number between 1 and 6 (inclusive)
     var dice = Math.floor(Math.random() * 6) + 1;
 
+    // Get the dice dom object
     var diceDom = document.querySelector(".dice");
 
+    // Display the dice and the correct dice image
     diceDom.style.display = "block";
     diceDom.src = "dice-" + dice + ".png";
 
+    // Check if the player rolled a 1
     if (dice > 1) {
+        // Increment round score and display it
         roundScore += dice
         document.querySelector("#current-" + activePlayer).textContent = roundScore;
     } else {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-
-        document.getElementById("current-0").textContent = "0";
-        document.getElementById("current-1").textContent = "0";
-
-        document.querySelector(".player-0-panel").classList.toggle("active");
-        document.querySelector(".player-1-panel").classList.toggle("active");
-    
-        document.querySelector(".dice").style.display = "none";
+        // Pass the turn
+        nextPlayer();
     }
 });
 
+document.querySelector(".btn-hold").addEventListener("click", function() {
+    // Increment players score by round score
+    scores[activePlayer] += roundScore;
 
+    // Display new score
+    document.querySelector("#score-" + activePlayer).textContent = scores[activePlayer];
+
+    // Check if the player won
+    if (scores[activePlayer] >= 100) {
+        // Display winner and appropriate styling
+        document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+        document.querySelector(".dice").style.display = "none";
+        document.querySelector(".player-" + activePlayer + "-panel").classList.add("winner");
+        document.querySelector(".player-" + activePlayer + "-panel").classList.remove("active");
+    }
+    else {
+        // Pass the turn to the next player
+        nextPlayer();
+    }
+});
+
+function nextPlayer() {
+    // Toggle active player and reset round score
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    // Do the necessary UI things -->
+    document.getElementById("current-0").textContent = "0";
+    document.getElementById("current-1").textContent = "0";
+
+    document.querySelector(".player-0-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+
+    document.querySelector(".dice").style.display = "none";
+}
