da54ba190d1388e6b2ced6d9e24cc0355b9e21ea
Odefus <otto.koivisto@gmail.com>
2018-02-22 13:51:20 +0200

    more structuring and input retrieval

--- Budgety/app.js
+++ Budgety/app.js
@@ -4,15 +4,39 @@ var budgetController = (function() {
 
 var UIController = (function() {
 
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        inputBtn: ".add__btn"
+    }
+
+    return {
+        getinput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    }
+
 })();
 
 var controller = (function(budgetCtrl, UICtrl) {
 
+    var DOM = UIController.getDOMstrings;
+
     var ctrlAddItem = function() {
-        console.log("HELLO");
+        var input = UIController.getinput();
+        console.log(input);
     }
 
-    document.querySelector(".add__btn").addEventListener("click", ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener("click", ctrlAddItem);
 
     document.addEventListener("keypress", function(event) {
         if (event.key === "Enter") {
