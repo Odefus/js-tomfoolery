a744767792650984802dc51a3386797e7dc91a36
Odefus <otto.koivisto@gmail.com>
2018-02-22 10:55:14 +0200

    Closures

--- Advanced-JS/script.js
+++ Advanced-JS/script.js
@@ -128,7 +128,6 @@ function game() {
     console.log(score >= 5);
 }
 game();
-*/
 
 (function () {
     var score = Math.random() * 10;
@@ -139,4 +138,44 @@ game();
     var score = Math.random() * 10;
     console.log(score >= 5 - goodLuck);
 })(5);
+*/
 
+// Closures
+
+function retirement(retirementAge) {
+    var a = " years left until retirement.";
+    return function(yearOfBirth) {
+        var age = 2018 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    } 
+}
+
+var retirementUS = retirement(66);
+var retirementFI = retirement(80);
+var retirementGR = retirement(65);
+
+retirementUS(1990);
+retirementFI(1990);
+retirementGR(1990);
+
+// retirement(66)(1990);
+
+function interviewQuestion(job) {
+    return function(name) {
+        if (job === "designer") {
+            console.log(name + ", can you please explain what UX design is?");
+        } else if (job === "teacher") {
+            console.log("What subject do you teach, " + name + "?");
+        } else {
+            console.log("Hello " + name + ", what do you do?");
+        }
+    }
+}
+
+iqDesigner = interviewQuestion("designer");
+iqTeacher = interviewQuestion("teacher");
+iqOther = interviewQuestion("other");
+
+iqDesigner("John");
+iqTeacher("John");
+iqOther("John");
