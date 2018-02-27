ff966f1a33fdaedc571776e8c710105600778e27
Odefus <otto.koivisto@gmail.com>
2018-02-27 13:39:39 +0200

    finally understanding map and reduce

--- ES6-Concepts/app.js
+++ ES6-Concepts/app.js
@@ -31,29 +31,44 @@ class Park extends Entity {
 
 class EOR {
     constructor() {
-        this.parks = new Map();
-        this.streets = new Map();
+        this.parks = [];
+        this.parkAvgAge = 0;
+
+        this.streets = [];
+        this.streetLengthSum = 0;
+        this.streetAvgLen = 0;
     }
 
-    addPark(key, obj) {
-        this.parks.set(key, obj);
+    addPark(obj) {
+        this.parks.push(obj);
     }
 
-    addStreet(key, obj) {
-        this.streets.set(key, obj);
+    calcParkAvgAge() {
+
+        this.parkAvgAge = this.parks
+            .map(p => p.calcAge())
+            .reduce((a, b) => a + b, 0)
+            / this.parks.length;
+
+            console.log(this.parkAvgAge);
+    }
+
+    addStreet(obj) {
+        this.streets.push(obj);
     }
 }
 
 const eor = new EOR();
 
 // Creating and adding the parks to the reporter
-eor.addPark(greenPark, new Park("Green Park", 1972, 900, 6));
-eor.addPark(nationalPark, new Park("National Park", 1962, 2000, 5));
-eor.addPark(OakPark, new Park("Oak Park", 1895, 400, 1));
+eor.addPark(new Park("Green Park", 1972, 900, 6));
+eor.addPark(new Park("National Park", 1962, 2000, 5));
+eor.addPark(new Park("Oak Park", 1895, 400, 1));
 
 // Creating and adding the streets to the reporter
-eor.addStreet(oceanStreet, new Street("Ocean Avenue", 1999, 3, "big"));
-eor.addStreet(evergreenStreet, new Street("Evergreen Street", 2006, 1, "small"));
-eor.addStreet(fourthStreet, new Street("4th Street", 2015, 2));
-eor.addStreet(sunsetStreet, new Street("Sunset Boulevard", 1982, 5, "huge"));
+eor.addStreet(new Street("Ocean Avenue", 1999, 3, "big"));
+eor.addStreet(new Street("Evergreen Street", 2006, 1, "small"));
+eor.addStreet(new Street("4th Street", 2015, 2));
+eor.addStreet(new Street("Sunset Boulevard", 1982, 5, "huge"));
 
+console.log(eor.calcParkAvgAge());
